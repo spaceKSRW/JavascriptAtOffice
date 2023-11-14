2659b0ef07f86c8ef691499e5b00246841eee21d
Karan Singh Rawat <karansinghrawat@Karans-MacBook-Air.local>
2023-11-14 16:43:30 +0530

    14nov 2

--- classObjects.js
+++ classObjects.js
@@ -1,6 +1,8 @@
 // the below code explains classes and objects and the
 // use of getters and setters method , use of constructors
 
+const { compact } = require("lodash");
+
 // class Person{
 //     constructor(name,gender)
 //     {
@@ -28,26 +30,133 @@
 
 // inheritance and super keyword use
 
-class Teacher {
-  constructor(name, subject) {
-    this.name = name;
-    this.subject = subject;
+// class Teacher {
+//   constructor(name, subject) {
+//     this.name = name;
+//     this.subject = subject;
+//   }
+//   get getInfo() {
+//     return `${this.name} teaches this ${this.subject}`;
+//   }
+// }
+// class Student extends Teacher {
+//   constructor(name , subject) 
+//   {
+//     super();
+//     this.name=name;
+//     this.subject = subject;
+//   }
+//   show(){
+//     return this.getInfo
+//   }
+// }
+
+// let user = new Student("karan","maths");
+// console.log(user.show())
+
+// function bankAccount(name)
+// {
+//   this.name=name;
+//   this.accno=Date.now()
+// }
+
+// const user1 = new bankAccount("karan");
+// console.log(user1)
+
+// bankAccount.prototype.fetchDetails=function(){
+//   console.log(`${this.name} has moneyyyy $$$ `)
+// }
+// user1.fetchDetails();
+
+// class BankAccount{
+//   customerName;
+//   accountNumber=Date.now();
+//   #balance=0;  // private field
+
+//   constructor(customerName)
+//   {
+//     this.customerName=customerName;
+//     this.accountNumber=Date.now();
+    
+//   }
+//   setBalance(amount)
+//   {
+//     this.#balance=amount;
+//   }
+//   deposit(amount){
+//    this.#balance+=amount;
+//   }
+//   withdraw(amount)
+//   {
+//     this.#balance-=amount;
+//   }
+//   showDetails(){
+//     return `${this.accountNumber} has name ${this.customerName} having balance ${this.#balance}`;
+//   }
+// }
+
+// class currentAccount extends BankAccount{
+//   transactionLimit=50000;
+//   constructor(customerName)
+//   {
+//     super(customerName);
+//   }
+//   takeBusinessLoan(amount)
+//   {
+//     console.log("taking business loan "+amount);
+//   }
+// }
+
+// class savingsAccount extends BankAccount{
+//   transactionLimit=10000;
+//   #id=90  // private field
+//   constructor(customerName,balance=0)
+//   {
+//     super(customerName);
+//   }
+//   takePersonalLoan(amount)
+//   {
+//     console.log("taking personal loan "+amount);
+//   }
+// }
+// let user=  new savingsAccount("karan",10000)
+
+// user.deposit(123232);
+// user.setBalance(1000000);
+// console.log(user.showDetails())
+
+class User{
+  static id={
+    val:1,
   }
-  get getInfo() {
-    return `${this.name} teaches this ${this.subject}`;
-  }
-}
-class Student extends Teacher {
-  constructor(name , subject) 
+  
+  constructor(age,name)
   {
-    super();
     this.name=name;
-    this.subject = subject;
+    this.age=age;
   }
-  show(){
-    return this.getInfo
-  }
+  static compareByAge(ob1,ob2)
+  {
+    return ob1.age-ob2.age;
+   }
+
+   static incrementID(){
+    console.log(this.id)
+   }
 }
 
-let user = new Student("karan","maths");
-console.log(user.show())
+const u1 = new User(34,"karan")
+const u2 = new User(35,"jane doe")
+const u3 = new User(21,"taylor");
+const users = [u1,u2,u3];
+User.id.val++;
+User.incrementID(); // static method accessing static property
+
+User.id.val++
+User.incrementID()
+
+
+
+users.sort(User.compareByAge)
+console.log(users)
+
--- mfr.js
+++ mfr.js
@@ -19,29 +19,59 @@
 // }
 // console.log(names);
 
-const li = [
-  ["karan", 22, "M"],
-  ["amit", 23, "M"],
-  ["aditya", 23, "M"],
-  ["aditya", 22, "M"],
-  ["karan", 21, "M"],
-  ["karan", 29, "M"],
-];
+// const li = [
+//   ["karan", 22, "M"],
+//   ["amit", 23, "M"],
+//   ["aditya", 23, "M"],
+//   ["aditya", 22, "M"],
+//   ["karan", 21, "M"],
+//   ["karan", 29, "M"],
+// ];
 
-let newobj = li.reduce((customer, line) => {
-  if (customer[line[0]]) {
-    customer[line[0]].push({
-      age: line[1],
-      gender: line[2],
-    });
-  } else {
-    customer[line[0]] = [];
-    customer[line[0]].push({
-      age: line[1],
-      gender: line[2],
-    });
-  }
+// let obj={}
+// let newobj = li.map((ele,ind,li)=>{
+//     if(obj[ele[0]]){
+//       obj[ele[0]].push({
+//         age:ele[1],
+//         gender:ele[2]
+//       })
+//     }
+//     else{
+//       obj[ele[0]]=[{age:ele[1],gender:[ele[2]]}]
+//     }
+// })
+// console.log(obj)
 
-  return customer;
-}, {});
-console.log(newobj);
+
+
+// let newobj = li.reduce((customer, line) => {
+//   if (customer[line[0]]) {
+//     customer[line[0]].push({
+//       age: line[1],
+//       gender: line[2],
+//     });
+//   } else {
+//     customer[line[0]] = [];
+//     customer[line[0]].push({
+//       age: line[1],
+//       gender: line[2],
+//     });
+//   }
+
+//   return customer;
+// }, {});
+// console.log(newobj);
+const promise = new Promise((res,rej)=>{
+  setTimeout(()=>{
+    res("task comeplete");
+  },3000)
+})
+console.log(promise)
+promise.then((data)=>{
+  console.log(data)
+}
+)
+
+setTimeout(()=>{
+  console.log(promise)
+},4000)
